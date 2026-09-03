8e19d22a1ba3c05f117f95c8933f0610499383e6
Nokuphiwa <shangenokuphiwa683@gmail.com>
2026-09-03 15:17:24 +0200

    update

--- src/controllers/items.ts
+++ src/controllers/items.ts
@@ -2,7 +2,7 @@ import { Item } from "../types/items";
 
 let Items: Item[] = [];
 
-let currentId = 0;
+let currentId = 1;
 
 export const getItems = (): Item[] => {
   return Items;
@@ -17,5 +17,17 @@ export const getItemById = (id: number): Item | undefined => {
 export const addItem = (name: string, quantity: number, price: number): Item => {
     const newItem: Item = {id: currentId++, name, quantity, price};
     Items.push(newItem);
+    currentId++;
     return newItem;
+}
+
+export const updateItem = (id: number, updates: Partial<Omit<Item, "id">>): Item | undefined =>{
+const item = getItemById(id);
+if(!item) return undefined;
+
+if(updates.name !== undefined) item.name = updates.name;
+if(updates.price !== undefined) item.price = updates.price;
+if(updates.quantity !== undefined) item.quantity = updates.quantity;
+
+return item;
 }
--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,12 +1,17 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { getItems, getItemById, addItem } from "../controllers/items";
+import {
+  getItems,
+  getItemById,
+  addItem,
+  updateItem,
+} from "../controllers/items";
 
 export const itemsRoute = async (req: IncomingMessage, res: ServerResponse) => {
   if (req.url?.startsWith("/items")) {
     console.log(req.url, "request url");
 
     const parts = req.url?.split("/");
-    console.log(parts, "request url parts");
+    // console.log(parts, "request url parts");
 
     const id = parts[2] ? parseInt(parts[2]) : undefined;
 
@@ -16,27 +21,62 @@ export const itemsRoute = async (req: IncomingMessage, res: ServerResponse) => {
       return;
     }
     if (req.method === "GET" && id) {
+      // const item = getItemById(id);
+      // res.writeHead(item? 200: 404, { 'Content-Type': 'application/json' });
+      // res.end(JSON.stringify(item || { message: "Item not found" }));
+      // return;
+      if (!isNaN(id)) {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Invalid item Id" }));
+        return;
+      }
       const item = getItemById(id);
-      res.writeHead(item ? 200 : 404, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(item || { message: "Item not found" }));
-      return;
+      if (!item) {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ error: "Item not found" }));
+        return;
+      }
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(item));
+        return;
     }
-
     if (req.method === "POST") {
-      await new Promise<void>((resolve) => {
-        let body = "";
-        req.on("data", (chunk) => {
-          body += chunk.toString();
-        });
-        req.on("end", () => {
-          const { name, quantity, price } = JSON.parse(body);
-          const newItem = addItem(name, quantity, price);
-          res.writeHead(201, { "Content-Type": "application/json" });
-          res.end(JSON.stringify(newItem));
-          resolve();
-        });
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+        console.log(body, "body");
+      });
+      req.on("end", () => {
+        const { name, quantity, price } = JSON.parse(body);
+        const newItem = addItem(name, quantity, price);
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(newItem));
       });
       return;
     }
+    if (req.method === "PUT" && id) {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        const updates = JSON.parse(body);
+        const updatedItem = updateItem(id, updates);
+
+        if (!updatedItem) {
+          res.writeHead(404, { "content-type": "application/json" });
+          res.end(JSON.stringify({ message: "Not found" }));
+          return;
+        }
+
+        res.writeHead(200, { "content-type": "application/json" });
+        res.end(JSON.stringify(updatedItem));
+      });
+      return;
+    }
+
+    res.writeHead(405, { "content-type": "application/json" });
+    res.end(JSON.stringify({ error: "Method not allowed on /items" }));
   }
 };
